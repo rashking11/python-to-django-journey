1fa7cd735316e2d7072ba8a9faa079ae38c2bf48
KingTech <rashking11@gmail.com>
2026-02-22 04:04:06 +0600

    Lesson 1: Basic Dictionary Structure

---
+++ week1_python_foundation/dictionaries/lesson1_basic_dictionary.py
@@ -0,0 +1,11 @@
+#Lesson 1: Basic Dictionary Structure
+
+customer = {
+    "name": "Rashid",
+    "account_number": "ACC1001",
+    "balance": 5000
+}
+
+print("Customer name:", customer["name"])
+print("Account number:", customer["account_number"])
+print("Balance:", customer["balance"])
