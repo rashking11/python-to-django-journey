34d034d6478c7a2939def3e2f745f6713eb13ab0
KingTech <rashking11@gmail.com>
2026-02-24 04:25:33 +0600

    Lesson 5: List of dictionaries

---
+++ week1_python_foundation/dictionaries/lesson5_list_of_dictionaries.py
@@ -0,0 +1,13 @@
+customers = [
+    {"id": "C001", "name": "Rashid", "balance": 5000},
+    {"id": "C002", "name": "Kofi", "balance": 3000},
+    {"id": "C003", "name": "Ama", "balance": 9000}
+]
+
+for customer in customers:
+    print("Customer ID: ", customer["id"])
+    print("Name: ", customer["name"])
+    print("Balance: ", customer["balance"])
+    print("_" * 30)
+
+    
