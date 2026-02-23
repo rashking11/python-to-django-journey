1a318ec80d34d8ebdea50d850539b6dbc8ad18ce
KingTech <rashking11@gmail.com>
2026-02-24 04:28:17 +0600

    Lesson 5 updated

--- week1_python_foundation/dictionaries/lesson5_list_of_dictionaries.py
+++ week1_python_foundation/dictionaries/lesson5_list_of_dictionaries.py
@@ -1,7 +1,7 @@
 customers = [
     {"id": "C001", "name": "Rashid", "balance": 5000},
     {"id": "C002", "name": "Kofi", "balance": 3000},
-    {"id": "C003", "name": "Ama", "balance": 9000}
+    {"id": "C003", "name": "Ama", "balance": 7000}
 ]
 
 for customer in customers:
