4da6b5cc7bdf54c362d8d8a860a9342f22f1998b
KingTech <rashking11@gmail.com>
2026-02-25 04:13:51 +0600

    OOP lesson 1: basic class

---
+++ week1_python_foundation/oop/lesson1_basic_class.py
@@ -0,0 +1,11 @@
+#Lesson 1: Basic class and objects
+
+class BankAccount:
+    pass
+
+#Creating objects (Instances)
+account1 = BankAccount()
+account2 = BankAccount()
+
+print(account1)
+print(account2)
